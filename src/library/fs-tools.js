c59243dd7e735ab39b607f84d533100a6573cb28
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-01-13 09:56:10 +0200

    Update fs-tools.js

--- src/library/fs-tools.js
+++ src/library/fs-tools.js
@@ -3,9 +3,11 @@ import { dirname, join } from "path";
 import fs from "fs-extra";
 
 const { readJSON, writeJSON, writeFile, createReadStream } = fs;
+// console.log("test", join(dirname(fileURLToPath(import.meta.url)), "."));
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
 const publicFolderPath = join(process.cwd(), "./public/img/users");
+// console.log(join(publicFolderPath, "../.."));
 
 console.log("ROOT OF THE PROJECT:", process.cwd());
 console.log("PUBLIC FOLDER:", publicFolderPath);
